37f8cd8deeeb4778825c988572bb3125a15df48a
abanoubmilad <abanoubcs@gmail.com>
2020-09-15 00:37:27 +0200

    Update sample app

--- app/src/main/java/org/abanoubmilad/router/MainActivity.kt
+++ app/src/main/java/org/abanoubmilad/router/MainActivity.kt
@@ -1,11 +1,38 @@
 package org.abanoubmilad.router
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+         *   listen to router
+         */
+        listenToRouter(viewModel.router)
+
+        /*
+        *   handle click event
+        *   or use data binding
+        */
+
+        btn_github_chrome_tab.setOnClickListener {
+            viewModel.onGithubChromeTabClicked()
+        }
+
+        btn_github_uri.setOnClickListener {
+            viewModel.onGithubUriClicked()
+        }
+
+        btn_play_store.setOnClickListener {
+            viewModel.onPlayStoreClicked()
+        }
     }
 }
---
+++ app/src/main/java/org/abanoubmilad/router/MainViewModel.kt
@@ -0,0 +1,32 @@
+package org.abanoubmilad.router
+
+import androidx.lifecycle.ViewModel
+import org.abanoubmilad.router.routes.ChromeTabRoute
+import org.abanoubmilad.router.routes.PlayStoreRoute
+import org.abanoubmilad.router.routes.UriRoute
+
+class MainViewModel : ViewModel() {
+    val router = Router()
+
+
+    fun onGithubChromeTabClicked() {
+        /*
+        *   create a route to start a chrome tab
+        */
+        router.routeTo(ChromeTabRoute("https://github.com/abanoubmilad"))
+    }
+
+    fun onGithubUriClicked() {
+        /*
+        *   create a route to view uri
+        */
+        router.routeTo(UriRoute("https://github.com/abanoubmilad"))
+    }
+
+    fun onPlayStoreClicked() {
+        /*
+        *   create a route to view an app on play store
+        */
+        router.routeTo(PlayStoreRoute("abanoubm.dayra"))
+    }
+}
